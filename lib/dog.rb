3a85e7bd6c8d1d76c5da5bc6edfc7ddb5ba0b8ec
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-16 12:07:38 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+    @@all = []
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        names = @@all.map { |dog| dog.name }
+        puts names.join("\n")
+    end
+
+    def save
+        @@all << self
+    end
+end
